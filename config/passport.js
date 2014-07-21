36af699cb4305c5b1939d3d4f344c2d3c203b0e2
lordhelmut <lordhelmut@gmail.com>
2014-07-21 15:48:39 -0700

    Better logic for twitter authorizations

--- config/passport.js
+++ config/passport.js
@@ -39,46 +39,17 @@ module.exports = function(passport) {
 			  if (err.notFound) {
 				db.put(req.user + ':twitter-token',  token, function(err) {
 				if (err) console.log(err);
-				return console.log('added user twitter tokens');
+				console.log('added user twitter tokens');
+				return done(null, req.user);
 				})
-				//console.log('Twitter token not found:',req.user);
-				//return;
 				}
 			  //some other error
 			  console.log('IO error',err);
+			  return done(null, false);
 			};
+			//console.log('what value?',value);
+			return done(null, req.user);
 		})
-
-		/*
-	  // make the code asynchronous
-	  // User.findOne won't fire until we have all our data back from Twitter
-	  process.nextTick(function() {
-			User.findOne({ 'twitter.id' : profile.id }, function(err, user) {
-				// if there is an error, stop everything and return that
-				// ie an error connecting to the database
-			    if (err)
-				return done(err);
-				// if the user is found then log them in
-			    if (user) {
-				return done(null, user); // user found, return that user
-			    } else {
-				// if there is no user, create them
-				var newUser                 = new User();
-				// set all of the user data that we need
-				newUser.twitter.id          = profile.id;
-				newUser.twitter.token       = token;
-				newUser.twitter.username    = profile.username;
-				newUser.twitter.displayName = profile.displayName;
-				// save our user into the database
-				newUser.save(function(err) {
-				    if (err)
-					throw err;
-				    return done(null, newUser);
-				});
-			    }
-			});
-		});
-		*/	
 
 	    }));
 
